c4cac58eaaea3991893acea0b2c0a87721e9281d
The-Huginn <rbudinsk@redhat.com>
2022-10-21 21:51:25 +0200

    Add API annotations for RestAdminService.

--- src/main/java/restfulAPI/RestAdminService.java
+++ src/main/java/restfulAPI/RestAdminService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -31,7 +32,7 @@ import helper.IResponseBuilder;
 @Stateless
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Api(value = "Users' Administration")
+@Api(value = "Student, Lector, Room and Course Administration")
 @RolesAllowed("admin")
 public class RestAdminService {
 
@@ -95,109 +96,164 @@ public class RestAdminService {
 	
 	@Path("/lector")
 	@POST
-	public Response createLector(Lector lector) {
+	@ApiOperation(value = "Creates new lector with initial password. This password can be changed at public API")
+	public Response createLector(
+        @ApiParam(required = true) Lector lector,
+        @ApiParam(required = true, example = "cGFzc3dvcmQxMjM=", value = "Expecting Base64 encoded password, e.g. password123 = cGFzc3dvcmQxMjM=") @QueryParam("password") String password) {
+        
+        String reply = adminSecurityService.addUser(lector.getUsername(), password);
+        if (reply != null)
+            return rb.createResponse(reply);
+
         return rb.createResponse(adminService.createLector(lector));
 	}
 	
 	@Path("/student")
 	@POST
-	public Response createStudent(Student student) {
+    @ApiOperation(value = "Creates new student with initial password. This password can be changed at public API")
+	public Response createStudent(
+        @ApiParam(required = true) Student student,
+        @ApiParam(required = true, example = "cGFzc3dvcmQxMjM=", value = "Expecting Base64 encoded password, e.g. password123 = cGFzc3dvcmQxMjM=") @QueryParam("password") String password) {
+        
+        String reply = adminSecurityService.addUser(student.getUsername(), password);
+        if (reply != null)
+            return rb.createResponse(reply);
+
         return rb.createResponse(adminService.createStudent(student));
 	}
 
     @Path("/room")
 	@POST
-	public Response createRoom(Room room) {
+    @ApiOperation(value = "Creates room")
+	public Response createRoom(
+        @ApiParam(required = true) Room room
+        ) {
         return rb.createResponse(adminService.createRoom(room));
 	}
 
 	@Path("/guarant/{course_uid}/{guarant_uid}")
 	@POST
-	public Response addGuarant(@PathParam("course_uid") long course_uid, @PathParam("guarant_uid") long guarant_uid) {
+    @ApiOperation(value = "Assigns Lector as guarant to course")
+	public Response addGuarant(
+        @ApiParam(required = true, example = "10") @PathParam("course_uid") long course_uid,
+        @ApiParam(required = true, example = "10") @PathParam("guarant_uid") long guarant_uid
+        ) {
         return rb.createResponse(adminService.addGuarant(course_uid, guarant_uid));
 	}
 
     @Path("/course")
     @GET
+    @ApiOperation(value = "Finds all available courses")
     public List<StudyCourse> getCourses() {
         return adminService.getCourses();
     }
 
     @Path("/course/{course_uid}")
     @GET
-    public StudyCourse getCourse(@PathParam("course_uid") long course_uid) {
+    @ApiOperation(value = "Finds course with specified uid")
+    public StudyCourse getCourse(
+        @ApiParam(required = true, example = "10") @PathParam("course_uid") long course_uid
+        ) {
         return adminService.getCourse(course_uid);
     }
 
     @Path("/student")
     @GET
+    @ApiOperation("Finds all existing students")
     public List<Student> getStudents() {
         return adminService.getStudents();
     }
 
     @Path("/student/{student_uid}")
     @GET
-    public Student getStudent(@PathParam("student_uid") long student_uid) {
+    @ApiOperation(value = "Finds student with specified uid")
+    public Student getStudent(
+        @ApiParam(required = true, example = "10") @PathParam("student_uid") long student_uid
+        ) {
         return adminService.getStudent(student_uid);
     }
 
     @Path("/lector")
     @GET
+    @ApiOperation(value = "Finds all existing lectors")
     public List<Lector> getLectors() {
         return adminService.getLectors();
     }
 
     @Path("/lector/{lector_uid}")
     @GET
-    public Lector getLector(@PathParam("lector_uid") long lector_uid) {
+    @ApiOperation(value = "Finds lector with specified uid")
+    public Lector getLector(
+        @ApiParam(required = true, example = "10") @PathParam("lector_uid") long lector_uid
+        ) {
         return adminService.getLector(lector_uid);
     }
 
     @Path("/room")
     @GET
+    @ApiOperation(value = "Finds all existing rooms")
     public List<Room> getRooms() {
         return adminService.getRooms();
     }
 
     @Path("/room/{room_uid}")
     @GET
-    public Room getRoom(@PathParam("room_uid") long room_uid) {
+    @ApiOperation("Finds room with specified uid")
+    public Room getRoom(
+        @ApiParam(required = true, example = "10") @PathParam("room_uid") long room_uid
+        ) {
         return adminService.getRoom(room_uid);
     }
 
     @Path("/guarant")
     @GET
+    @ApiOperation(value = "Finds all lectors who are guarants at least for one subject")
     public List<Lector> getGuarants() {
         return adminService.getGuarants();
     }
 
     @Path("/guarant/{course_uid}")
     @GET
-    public Lector getGuarant(@PathParam("course_uid") long course_uid) {
+    @ApiOperation(value = "Finds guarant for course specified with uid")
+    public Lector getGuarant(
+        @ApiParam(required = true, example = "10") @PathParam("course_uid") long course_uid
+        ) {
         return adminService.getGuarant(course_uid);
     }
 
     @Path("course/{course_uid}")
     @DELETE
-    public Response removeCourse(@PathParam("course_uid") long course_uid) {
+    @ApiOperation(value = "Removes course specified by uid")
+    public Response removeCourse(
+        @ApiParam(required = true, example = "10") @PathParam("course_uid") long course_uid
+        ) {
         return rb.createResponse(adminService.removeCourse(course_uid));
     }
 
     @Path("lector/{lector_uid}")
     @DELETE
-    public Response removeLector(@PathParam("lector_uid") long lector_uid) {
+    @ApiOperation(value = "Removes lector specified by uid")
+    public Response removeLector(
+        @ApiParam(required = true, example = "10") @PathParam("lector_uid") long lector_uid
+        ) {
         return rb.createResponse(adminService.removeLector(lector_uid));
     }
 
     @Path("student/{student_uid}")
     @DELETE
-    public Response removeStudent(@PathParam("student_uid") long student_uid) {
+    @ApiOperation(value = "Removes student specified by uid")
+    public Response removeStudent(
+        @ApiParam(required = true, example = "10") @PathParam("student_uid") long student_uid
+        ) {
         return rb.createResponse(adminService.removeStudent(student_uid));
     }
 
     @Path("room/{room_uid}")
     @DELETE
-    public Response removeRoom(@PathParam("room_uid") long room_uid) {
+    @ApiOperation(value = "Removes room specified by uid")
+    public Response removeRoom(
+        @ApiParam(required = true, example = "10") @PathParam("room_uid") long room_uid
+        ) {
         return rb.createResponse(adminService.removeRoom(room_uid));
     }
 }
