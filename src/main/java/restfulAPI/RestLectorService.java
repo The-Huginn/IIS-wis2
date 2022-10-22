9695ecbed08cb7f50643e37b04857f4d4cd69f68
The-Huginn <rbudinsk@redhat.com>
2022-10-22 15:41:18 +0200

    Refactor Rest API. Move code into separate service bean.

--- src/main/java/restfulAPI/RestLectorService.java
+++ src/main/java/restfulAPI/RestLectorService.java
@@ -1,18 +1,30 @@
 package restfulAPI;
 
+import java.util.List;
+
+import javax.annotation.Resource;
 import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJBContext;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import entity.DateEvaluation;
 import entity.Lector;
+import helper.IResponseBuilder;
+import io.swagger.annotations.ApiParam;
 
 @Path("/lector")
 @Stateless
@@ -23,17 +35,40 @@ public class RestLectorService {
 
 	@PersistenceContext(unitName = "primary")
 	EntityManager em;
+
+	@Resource
+	EJBContext ejb;
+
+	@Inject
+	private IResponseBuilder rb;
 	
-    @Path("/dateEval/add_evaluation")
+    @Path("/dateEval/{dateEval_uid}")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public void addGuarant(@QueryParam("dateEval_uid") long dateEval_uid,
-							@QueryParam("lector_uid") long lector_uid, @QueryParam("evaluation") double evaluation)
-	{
-		DateEvaluation dateEval = em.find(DateEvaluation.class, dateEval_uid);
-        Lector lector = em.find(Lector.class, lector_uid);
-		dateEval.setLector(lector);
-		dateEval.setEvaluation(evaluation);
-		em.persist(dateEval);
+	public Response addEvaluation(
+		@ApiParam(required = true, example = "10") @PathParam("dateEval_uid") long dateEval_uid,
+		@ApiParam(required = true, example = "69", value = "Value between 0-100")
+			@FormParam("evaluation")
+			@DecimalMin(value = "0.0", message = "Must be greater than 0.0 [RestLectorService.addEvaluation]")
+			@DecimalMax(value = "100.0", message = "Must be smaller than 100.0 [RestLectorService.addEvaluation]") double evaluation
+	) {
+		TypedQuery<Lector> query = em.createNamedQuery("findUid", Lector.class);
+		query.setParameter("table", "Lector").setParameter("username", ejb.getCallerPrincipal().getName());
+		List<Lector> reply = query.getResultList();
+		if (reply.isEmpty())
+			return rb.createResponse("User is not found as lector in database");
+		
+		Lector lector = reply.get(0);
+		try {
+			DateEvaluation dateEval = em.find(DateEvaluation.class, dateEval_uid);
+			dateEval.setLector(lector);
+			dateEval.setEvaluation(evaluation);
+			em.persist(dateEval);
+		} catch (Exception e) {
+			e.printStackTrace();
+			rb.createResponse(e.getMessage());
+		}
+
+		return rb.createResponse(null);
 	}
 }
