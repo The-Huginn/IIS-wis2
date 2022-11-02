92e3138838bf4a70aa1a53bac637c75f117d78ea
The-Huginn <rbudinsk@redhat.com>
2022-11-02 11:54:09 +0100

    Create environment for running server and database in containers. Based on publicly open registries.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# Create network link for database
+
 # Creates PostgreSQL database and runs in detached mode
 podman rm -f postgresql
 
@@ -9,18 +11,22 @@ podman run -d \
 --env POSTGRESQL_DATABASE=wis2db \
 --env TZ='GMT-2' \
 --env PGTZ='GMT-2' \
-registry.redhat.io/rhel8/postgresql-10:latest
-
-# Builds our project
-mvn clean package
+registry.access.redhat.com/rhscl/postgresql-10-rhel7:latest
 
 # Builds our own image for project
+mvn clean package
 podman build -t wis2 .
 
 # Runs our application in podman in attached mode
 podman rm -f wis2
 
 podman run \
--p 8080:8080 \
 --name wis2 \
+-p 8080:8080 \
+--env DB_USER=wis2admin \
+--env DB_PWD=wis2Admin \
+--env DB_DRIVER=postgresql \
+--env DB_HOST=192.168.122.1 \
+--env DB_PORT=5432 \
+--env DB_DB=wis2db \
 wis2:latest
